badbfc350234ea901ee81c04d898102d00aac71b
blackndwhite <mateialexandrucristian95@gmail.com>
2022-09-16 13:33:13 +0300

    commit eventHandler

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -1,9 +1,15 @@
 function Trip(props) {
+  function deleteHandler() {
+    console.log("Clicked " + props.name);
+  }
+
   return (
     <div className="card">
       <h3>{props.name}</h3>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
     </div>
   );
